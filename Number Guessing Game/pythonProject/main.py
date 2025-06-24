326350fb787499bccf574fbabd07b1992b73a8ac
TeodorN16 <149152387+TeodorN16@users.noreply.github.com>
2025-06-24 19:46:00 +0300

    Completed

--- Number Guessing Game/pythonProject/main.py
+++ Number Guessing Game/pythonProject/main.py
@@ -1,2 +1,22 @@
-try:
-    n
+import random
+
+
+goagain=True
+while goagain==True:
+    try:
+        pickedNum=random.randrange(1,10)
+        numberGiven = int(input("Enter a number from 1 to 10: "))
+        if(pickedNum==numberGiven):
+            print("Congratulations you won!")
+            anotherGame = input("Play again?(y/n): ")
+            if(anotherGame!="y"):
+                break
+        else:
+            print("Ohh next time better")
+            anotherGame = input("Play again?(y/n): ")
+            if (anotherGame != "y"):
+                break
+
+
+    except:
+        print("Something went wrong....")
