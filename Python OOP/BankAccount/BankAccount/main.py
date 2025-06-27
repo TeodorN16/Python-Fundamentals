561e7f13aeaad3c52c048f2be4dda6e935999821
TeodorN16 <149152387+TeodorN16@users.noreply.github.com>
2025-06-27 16:51:53 +0300

    .

--- Python OOP/BankAccount/BankAccount/main.py
+++ Python OOP/BankAccount/BankAccount/main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
+class BankAccount:
+    def __init__(self, owner_name, balance):
+        self.owner_name = owner_name
+        self.balance = balance
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return "Insufficient funds..."
+        self.balance -= amount
+        return f"Withdrew {amount} from {self.owner_name}'s account."
+
+    def deposit(self, amount):
+        self.balance += amount
+        return f"Deposited {amount} to {self.owner_name}'s account."
+
+    def view_balance(self):
+        return f"The account has balance: {self.balance}"
+
+    def account_details(self):
+        return f"Bank Account of {self.owner_name} has balance: {self.balance}"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+owner = input("Enter owner's name: ")
+balance = int(input("Enter the default balance: "))
+bank_account = BankAccount(owner, balance)
+print("Bank account created!")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+while True:
+    action = input("Withdraw/Deposit/View/Details: ").lower()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if action == "withdraw":
+        amount = int(input("Enter the withdrawal amount: "))
+        print(bank_account.withdraw(amount))
+    if action=="view":
+        print(bank_account.view_balance())
+    if action=="details":
+        print(bank_account.account_details())
+
+    elif action == "deposit":
+        amount = int(input("Enter the deposit amount: "))
+        print(bank_account.deposit(amount))
+
+    else:
+        print("Invalid action. Please type 'Withdraw' or 'Deposit'.")
+        continue
+
+
+    go_again = input("Do more? (y/n): ").lower()
+
+    if go_again == "n":
+        print("Goodbye!")
+        break
+    elif go_again == "y":
+        continue
+    else:
+        print("Not a valid option. Ending session.")
+        break
